37d86f465663f8c7f54e4234a19234645b3039ac
teknoreceh <danaputrayuda@gmail.com>
2023-03-24 01:38:54 +0700

    nambah message ketika di run

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const http = require('http');
+const host = '127.0.0.1'
+const port = 3002
 // request data masuk dari luar
 // response data keluar dari server
-http.createServer(function (request, response){
-response.end('Halo pelajar..!')
-}).listen(3001)
+const server = http.createServer(function (request, response){
+response.end('Halo pelajar..!');
+});
 // jalankan dengan perintah node index.js lalu buka browser localhost:3001
+server.listen(port, host, '', function (){
+    console.log(`server berjalan dengan baik di ${host}:${port}`);
+})
