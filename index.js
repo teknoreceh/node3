0b639c4c4451d3f85b6b9e4168578e35974639c8
teknoreceh <danaputrayuda@gmail.com>
2023-03-24 01:42:44 +0700

    menambahkan status code

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const port = 3002
 // request data masuk dari luar
 // response data keluar dari server
 const server = http.createServer(function (request, response){
-response.end('Halo pelajar..!');
+    response.statusCode = 203;
+    response.end('Halo pelajar..! menambahkan status 203 ');
 });
 // jalankan dengan perintah node index.js lalu buka browser localhost:3001
 server.listen(port, host, '', function (){
